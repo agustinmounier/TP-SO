bbbc25d358400163c28c0f199d58811ff767716b
Melisa Anabella Rossi <melisaanabellarossi@MacBook-Air-de-Melisa.local>
2015-04-04 16:38:53 -0300

    h

---
+++ common/client.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "client.h"
+
+int
+main(void){
+	char input[15];
+	printf("Welcome to SO-cinema.\n");
+	while(1){
+		printf("Type the desired option: \n");
+		printf("ShowMovies, \n");
+		scanf("%s", input);
+		execute_input(input);
+	}
+}
+
+void
+execute_input(char * input){
+
+	int cmd = 0;
+	for(; cmd < CANT_COMMANDS; cmd++){
+		if(strcmp(input, commands[cmd]) == 0)
+			break;
+	}
+
+	switch(cmd){
+		case 0: show_movies();
+				break;
+		default: printf("Invalid option. \n");
+
+
+	}
+}
+
+void
+show_movies(){
+	printf("Pulp Fiction.\n");
+	printf("The Wolf of Wall Street. \n");
+	printf("Inception. \n");
+}
