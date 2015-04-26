489ca59532365bbb71b588258c8b4e60737d1520
Melisa Anabella Rossi <melisaanabellarossi@MacBook-Air-de-Melisa.local>
2015-04-26 19:21:06 -0300

    files

--- signalsfiles/src/filessig.h
+++ signalsfiles/src/filessig.h
@@ -1,8 +1,13 @@
-#define CLIENT_FILE_PATH "../../signalsfiles/clients/client_%ld"
-#define SERVER_PID_FILE "../../signalsfiles/server_pid"
+#ifndef _filessig_
+#define _filessig_
+
+#define CLIENT_FILE_PATH "../tmp/client_%ld"
+#define SERVER_PID_FILE "./server_pid"
 
 void communicate_with_server();
 void readClientMessage();
 void dealWithClient();
 void user1_handler();
 void user2_handler();
+
+#endif
